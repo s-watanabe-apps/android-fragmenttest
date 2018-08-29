04933138a0d577e498ff2359586c4ea805ee1d7e
Shin Watanabe <swatanabe.apps@gmail.com>
2018-08-30 00:06:45 +0900

    first commit

---
+++ app/src/main/java/com/example/fragmenttest/Main2Fragment.java
@@ -0,0 +1,28 @@
+package com.example.fragmenttest;
+
+import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+@EFragment(R.layout.fragment_main2)
+public class Main2Fragment extends Fragment {
+
+    @ViewById
+    TextView textView;
+
+    @ViewById
+    EditText editTest;
+
+    @ViewById
+    Button buttonTest;
+
+    @Click
+    void buttonTest() {
+        Toast.makeText(getContext(), editTest.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+}
