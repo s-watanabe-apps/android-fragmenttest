8ac443ec3b23bb64b4d17473967af7631a9b1b35
Shin Watanabe <swatanabe.apps@gmail.com>
2018-09-01 22:33:29 +0900

    ActivityからFragmentのメソッドを呼ぶ

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fragmenttest;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonTest1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment1);
+                if (fragment != null && fragment instanceof Main1Fragment) {
+                    ((Main1Fragment) fragment).buttonTest();
+                }
+            }
+        });
+
+        findViewById(R.id.buttonTest2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment2);
+                if (fragment != null && fragment instanceof Main2Fragment) {
+                    ((Main2Fragment) fragment).buttonTest();
+                }
+            }
+        });
     }
 }
